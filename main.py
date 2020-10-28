784463950d5a6d66d2256b9bc704bb0f1144afb8
Mateus Soares <fvs.mateus@gmail.com>
2020-10-28 15:06:14 -0300

    m - finalizado inserção

--- main.py
+++ main.py
@@ -1,19 +1,163 @@
 class Tree:
 
     def __init__(self):
-        self._node = Node()
+        self._root = None
 
+    @property
+    def root(self):
+        return self._root
+
+    @root.setter
+    def root(self, root):
+        self._root = root
+
+    def __setitem__(self, key, value):
+        #create node
+        node = Node(key, value)
+
+        aux_parent = None
+        aux_node = self.root
+
+        if aux_node is not None:
+            while aux_node.key is not None:
+                aux_parent = aux_node
+                if node.compareTo(aux_node) == 0:
+                    aux_node = aux_node.left
+                else:
+                    aux_node = aux_node.right
+
+        node.parent = aux_parent
+        if aux_parent is None:
+            self.root = node
+        elif node.compareTo(aux_parent) == 0:
+            aux_parent.left = node
+        else:
+            aux_parent.right = node
+
+        if node.parent is None:
+            node.color = 'black'
+            return
+
+        if node.get_avo() is None:
+            return
+
+        self.__fix_insert(node)
+
+    def __fix_insert(self, node):
+        while node.parent.color == 'red':
+            if node.parent == node.parent.parent.right:
+                tio = node.parent.parent.left
+                if tio.color == 'red':
+                    tio.color = 'black'
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self.__right_rotate(node)
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    self.__left_rotate(node.parent.parent)
+            else:
+                tio = node.parent.parent.right
+                if tio.color == 'red':
+                    tio.color = 'black'
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self.__left_rotate(node)
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    self.__right_rotate(node.parent.parent)
+
+            if node == self.root:
+                break
+        self.root.color = 'black'
+
+
+    def __left_rotate(self, node):
+        aux = node.right
+        node.right = aux.left
+        if aux.left.key is not None:
+            aux.left.parent = node
+
+        aux.parent = node.parent
+        if node.parent is None:
+            self.root = aux
+        elif node == node.parent.left:
+            node.parent.left = aux
+        else:
+            node.parent.right = aux
+        aux.left = node
+        node.parent = aux
+
+    def __right_rotate(self, node):
+        aux = node.left
+        node.left = aux.right
+        if aux.right.key is not None:
+            aux.right.parent = node
+
+        aux.parent = node.parent
+        if node.parent is None:
+            self.root = aux
+        elif node == node.parent.right :
+            node.parent.right = aux
+        else:
+            node.parent.left = aux
+        aux.right = node
+        node.parent = aux
+
+    def __in_order(self, node):
+        if node is None:
+            return
+        else:
+            self.__in_order(node.left)
+            print(node)
+            self.__in_order(node.right)
+
+    def __pre_order(self, node):
+        if node is None:
+            return
+        else:
+            print(node)
+            self.__pre_order(node.left)
+            self.__pre_order(node.right)
+
+    def __pos_order(self, node):
+        if node is None:
+            return
+        else:
+            self.__pre_order(node.left)
+            self.__pre_order(node.right)
+            print(node)
+
+    def print_in_order(self):
+        self.__in_order(self.root)
+
+    def print_pre_order(self):
+        self.__pre_order(self.root)
+
+    def print_pos_order(self):
+        self.__pos_order(self.root)
 
 class Node:
 
-    def __init__(self, key=None, value=None, left=None, right=None, color=None, parent=None):
+    def __init__(self, key=None, value=None, parent=None, color='red'):
 
         self._key = key
         self._value = value
-        self._left = left
-        self._right = right
-        self._color = color
+        if key is not None:
+            self._left = Node(color='black')
+            self._right = Node(color='black')
+        else:
+            self._left = None
+            self._right = None
         self._parent = parent
+        self._color = color
 
     @property
     def key(self):
@@ -67,11 +211,9 @@ class Node:
         return f'{self._key} --> {self._value} ({self._color})'
 
     def compareTo(self, other):
-        if other is None:
+        if other.key is None:
             return None
-        aux = [self._key, other.key]
-        aux.sort()
-        if aux[0] == self._key:
+        if self._key <= other.key:
             return 0
         else:
             return 1
@@ -92,5 +234,15 @@ class Node:
             return avo.right
 
 
-no = Node(color='black')
-print(no)
+arvore = Tree()
+
+arvore['a'] = 1
+arvore['b'] = 1
+arvore['c'] = 1
+arvore['d'] = 1
+arvore['e'] = 1
+arvore['f'] = 1
+arvore['g'] = 1
+arvore['h'] = 1
+
+arvore.print_in_order()
